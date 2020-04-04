de039ccad26ef07264d9301790f960f1509cb9f1
Edvin Norling <edvin.norling@gmail.com>
2020-04-04 15:59:16 +0200

    Adding initial templateing
    This to be able to test if i can set a url and use createrepo cmd

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/aws/aws-sdk-go v1.29.34
 	github.com/cavaliercoder/badio v0.0.0-20160213150051-ce5280129e9e // indirect
 	github.com/cavaliercoder/go-rpm v0.0.0-20200122174316-8cb9fd9c31a8
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
 )
---
+++ handler/homeHandler.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func HomeHandler(c echo.Context) error {
+	// Please note the the second parameter "home.html" is the template name and should
+	// be equal to the value stated in the {{ define }} statement in "view/home.html"
+	return c.Render(http.StatusOK, "home.html", map[string]interface{}{
+		"name": "HOME",
+		"msg":  "package1.rpm",
+		"msg2": "package2.rpm",
+	})
+}
